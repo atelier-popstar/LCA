2a7d033c3013981b7cab15427ab2af755be0bdbf
atelier-popstar <66830834+atelier-popstar@users.noreply.github.com>
2020-12-07 11:55:41 +0000

    Add files via upload

--- LCA/LCA.java
+++ LCA/LCA.java
@@ -3,15 +3,15 @@ import java.util.List;
  
 class Node { 
     int data; 
-    Node left, right; 
+    List<Node> successors; 
   
     Node(int value) { 
         data = value; 
-        left = right = null; 
+        successors = null; 
     } 
 } 
   
-public class BT_NoParentPtr_Solution1  
+public class LCA 
 { 
   
     Node root; 
@@ -61,13 +61,13 @@ public class BT_NoParentPtr_Solution1
             return true; 
         } 
   
-        if (root.left != null && findPath(root.left, n, path)) { 
-            return true; 
-        } 
-  
-        if (root.right != null && findPath(root.right, n, path)) { 
-            return true; 
-        } 
+        for(int i=0;i < root.successors.size();i++) {
+        	 Node t = root.successors.get(i);
+        	 if (t != null && findPath(t, n, path)) {
+        		 return true;
+        	 }
+        	
+        }
   
         // If not present in subtree rooted with root, remove root from 
         // path[] and return false 
@@ -79,19 +79,9 @@ public class BT_NoParentPtr_Solution1
     // Driver code 
     public static void main(String[] args) 
     { 
-        BT_NoParentPtr_Solution1 tree = new BT_NoParentPtr_Solution1(); 
-        tree.root = new Node(1); 
-        tree.root.left = new Node(2); 
-        tree.root.right = new Node(3); 
-        tree.root.left.left = new Node(4); 
-        tree.root.left.right = new Node(5); 
-        tree.root.right.left = new Node(6); 
-        tree.root.right.right = new Node(7); 
+        
   
-        System.out.println("LCA(4, 5): " + tree.findLCA(4,5)); 
-        System.out.println("LCA(4, 6): " + tree.findLCA(4,6)); 
-        System.out.println("LCA(3, 4): " + tree.findLCA(3,4)); 
-        System.out.println("LCA(2, 4): " + tree.findLCA(2,4)); 
+       
       
     } 
 }  
